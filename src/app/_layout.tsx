77bf9f495e1f3905dc92126a43204e07d37a6f85
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-06-05 12:22:41 -0300

    :wrench: chore: configure font family

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,21 @@
 import '@/styles/global.css'
 import { Slot } from 'expo-router'
+import {
+  useFonts,
+  Barlow_400Regular,
+  Barlow_500Medium,
+  Barlow_700Bold,
+} from '@expo-google-fonts/barlow'
+import Loading from '@/components/loading'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Barlow_400Regular,
+    Barlow_500Medium,
+    Barlow_700Bold,
+  })
+
   return (
-    <Slot />
+    fontsLoaded ? <Slot /> : <Loading />
   )
 }
---
+++ src/styles/fonts.ts
@@ -0,0 +1,5 @@
+export const fontFamily = {
+  regular: "Barlow_400Regular",
+  medium: "Barlow_500Medium",
+  bold: "Barlow_700Bold",
+}
