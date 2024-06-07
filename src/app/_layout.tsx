71f1f04df9e1a708a0071b4be221a6aee7a4423e
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-06-07 16:03:27 -0300

    :recycle: refactor: restyle login screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -35,6 +35,7 @@ export default function Layout() {
         }}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="ship-details" options={{ title: 'Detalhes do Navio' }} />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
         </Stack>
       ) : (
         <Loading />
--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,33 +1,38 @@
 import Button from "@/components/button";
-import Input from "@/components/input";
 import { colors } from "@/styles/colors";
 import { Link } from "expo-router";
 import { useState } from "react";
 import { Image, Text, View } from "react-native";
-import Checkbox from 'expo-checkbox'
-import image from '@/assets/title.png'
+import Checkbox from 'expo-checkbox';
+import appTitleImg from '@/assets/app-title.png';
+import { Input } from "@/components/input";
 
 export default function Login() {
   const [rememberPass, setRememberPass] = useState<boolean>(false)
 
   return (
-    <View className="flex-1 bg-gray-100 justify-center items-center gap-16 p-8">
+    <View className="flex-1 bg-dark_blue justify-center items-center gap-16 p-8">
       <Image
-        source={image}
-        className="h-24 border w-full"
+        source={appTitleImg}
+        className="h-8 w-full"
         resizeMode="contain"
       />
 
       <View className="gap-8 w-full">
-        <Text className="text-center font-bold text-dark">
+        <Text className="text-center font-bold text-lg text-gray-100">
           Acesse sua conta
         </Text>
 
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
+        <Input.Root>
+          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+        </Input.Root>
+
+        <Input.Root>
+          <Input.Field placeholder="Senha" keyboardType="visible-password" />
+        </Input.Root>
 
         <View className="flex-row justify-between">
-          <Text>
+          <Text className="text-gray-100">
             Esqueci minha senha
           </Text>
 
@@ -35,9 +40,12 @@ export default function Login() {
             <Checkbox
               value={rememberPass}
               onValueChange={setRememberPass}
-              color={rememberPass ? colors.dark : undefined}
+              color={colors.blue}
+
             />
-            <Text>Lembre-me</Text>
+            <Text className="text-gray-100">
+              Lembre-me
+            </Text>
           </View>
         </View>
 
@@ -45,7 +53,7 @@ export default function Login() {
 
         <Text className="text-center text-gray-500">
           Ainda não tem uma conta?{' '}
-          <Link href='/register' className="text-sky-800 font-bold">
+          <Link href='/register' className="text-blue font-bold">
             Cadastre-se
           </Link>
         </Text>
